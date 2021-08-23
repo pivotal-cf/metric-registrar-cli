493160613c5207b170ab8b76a9bd8cfea65b3f60
Benjamin Fuller <benjaminf@vmware.com>
2021-08-23 20:00:00 +0000

    reinstall now installs on linux
    - most of the cf devs are using linux now
    
    Co-authored-by: Michael Oleske <moleske@vmware.com>

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -4,6 +4,6 @@ set -e
 cf uninstall-plugin metric-registrar || true # suppress errors
 
 cd "$(dirname $0)/.."
-CLI_BUILD_OS=darwin CLI_BUILD_ARCH=amd64 ./scripts/build.sh
+CLI_BUILD_OS=linux CLI_BUILD_ARCH=amd64 ./scripts/build.sh
 
-cf install-plugin ./plugins/metric-registrar-cli-darwin-amd64-* -f
+cf install-plugin ./plugins/metric-registrar-cli-linux-amd64-* -f
