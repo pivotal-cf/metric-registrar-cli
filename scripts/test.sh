989e9efbc48edc6c87d630e5e23c383bbd33f18f
Carson Long <lcarson@vmware.com>
2022-03-21 09:58:54 -0700

    scripts/test uses ginkgo v2 CLI

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,5 @@
 set -exu
 
 pushd "$(dirname $0)/.."
-  go mod download
-  ${GOPATH}/bin/ginkgo -r --race --randomizeAllSpecs
+  go run github.com/onsi/ginkgo/v2/ginkgo -r --randomize-all --randomize-suites --fail-on-pending --keep-going --race --trace
 popd
